d90007677965b28fce0f184efb41f164e2c2aa63
mattcroat <mattcroat.dev@gmail.com>
2020-10-25 17:08:29 +0100

    TypeScript boilerplate

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const items = document.querySelectorAll('.item');
+// https://stackoverflow.com/questions/47585409/use-spread-operator-on-nodelist-in-typescript
+// const items = [...document.querySelectorAll('.item')]
+items.forEach(item => {
+    item.addEventListener('pointerdown', (event) => {
+        item.classList.add('dragging');
+    });
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+const items = document.querySelectorAll('.item') as NodeListOf<HTMLElement>
+
+// https://stackoverflow.com/questions/47585409/use-spread-operator-on-nodelist-in-typescript
+// const items = [...document.querySelectorAll('.item')]
+
+items.forEach(item => {
+  item.addEventListener('pointerdown', (event: PointerEvent) => {
+    item.classList.add('dragging')
+  })
+})
